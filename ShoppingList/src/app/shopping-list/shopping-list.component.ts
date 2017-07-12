ecd4dbb68cc6b3a0021a783afb085b85d3421401
Bartomiej Leraczyk <leraczykbartek@gmail.com>
2017-07-12 14:45:31 +0200

    Koniec tworzenia komponentów

--- ShoppingList/src/app/shopping-list/shopping-list.component.ts
+++ ShoppingList/src/app/shopping-list/shopping-list.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Ingredient} from '../shared/ingredient.model';
+
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
+  ingredients: Ingredient[]= [
+    new Ingredient('Pasta',10),
+    new Ingredient('Ziemniaki',50),
+    new Ingredient('Pizza',1)
+  ];
   constructor() { }
 
   ngOnInit() {
